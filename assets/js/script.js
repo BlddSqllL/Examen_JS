f7e77e772f689addcf9540259cf476f781643be5
BlddSqllL <rodrigo.aracena17@icloud.com>
2024-07-07 12:20:21 -0400

    carga de primera info

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+
+document.getElementById('buscar').addEventListener('click', async function() {
+    try {
+        var pesos = document.getElementById('pesos').value;
+        var currency = document.getElementById('currency').value;
+        var response = await fetch('https://mindicador.cl/api/' + currency);
+        var data = await response.json();
+        var result = pesos / data.serie[0].valor;
+        document.getElementById('result').textContent = result.toFixed(2);
+
+        var lastFiveDaysData = data.serie.slice(0, 5);
+        var dates = lastFiveDaysData.map(item => new Date(item.fecha).toLocaleDateString());
+        var values = lastFiveDaysData.map(item => item.valor);
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Valor de la moneda en los últimos 5 días',
+                    data: values,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
